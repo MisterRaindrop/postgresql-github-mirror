565aa6ccf49286e55bcbe47bf7e80dcff8867fd6
Peter Eisentraut <peter@eisentraut.org>
2026-09-16 09:21:56 +0200

    Rename PGAlignedXLogBlock to PGIOAlignedXLogBlock
    Before commit faeedbcefd4, PGAlignedXLogBlock was MAXALIGNed and a
    sibling of PGAlignedBlock.  That commit introduced PGIOAlignedBlock
    that was aligned as PG_IO_ALIGN_SIZE and *changed* PGAlignedXLogBlock
    to also be aligned as PG_IO_ALIGN_SIZE.  This creates an inconsistent
    naming, as there would now be no room for a MAXALIGNed block of size
    XLOG_BLCKSZ.  To improve this, rename PGAlignedXLogBlock to
    PGIOAlignedXLogBlock, which now correctly reflects its purpose.  A
    MAXALIGNed PGAlignedXLogBlock could be re-introduced later.

--- src/bin/pg_resetwal/pg_resetwal.c
+++ src/bin/pg_resetwal/pg_resetwal.c
@@ -1130,7 +1130,7 @@ KillExistingWALSummaries(void)
 static void
 WriteEmptyXLOG(void)
 {
-	PGAlignedXLogBlock buffer;
+	PGIOAlignedXLogBlock buffer;
 	XLogPageHeader page;
 	XLogLongPageHeader longpage;
 	XLogRecord *record;
